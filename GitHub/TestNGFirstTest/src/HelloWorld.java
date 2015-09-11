204462bd9c9e44211c96a8b4fc135053f5ac8f8d
Javed Chowdhury <betech.selenium@gmail.com>
2015-09-10 23:31:48 -0400

    Revert "Update Hello world file"
    This reverts commit 30947fc823e6768a8e53e3ea901559074ac6cc2e.

--- GitHub/TestNGFirstTest/src/HelloWorld.java
+++ GitHub/TestNGFirstTest/src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Update version");
+		Frame f = new JFrame("Content Pane Demo");
 
 	}
 
