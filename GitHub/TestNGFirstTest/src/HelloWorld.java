81ee0a1ecbab9f79ed05b7d7dc78e5a52236cfb0
Javed Chowdhury <betech.selenium@gmail.com>
2015-09-10 23:23:51 -0400

    Add java file

--- GitHub/TestNGFirstTest/src/HelloWorld.java
+++ GitHub/TestNGFirstTest/src/HelloWorld.java
@@ -1,8 +1,13 @@
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Frame f = new JFrame("Content Pane Demo");
 
 	}
 
